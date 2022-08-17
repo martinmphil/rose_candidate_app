4e166879c63b3ad4445180a328225e810f54d4ef
Martin <martin.mphil@gmail.com>
2022-08-17 09:24:05 +0100

    added clickable logout link

--- src/legitUser.ts
+++ src/legitUser.ts
@@ -1,7 +1,17 @@
 import { showDisciplines } from "./showDisciplines";
 import { showQText } from "./showQText";
 
+function clickableLogoutLinkInHeader() {
+  const node = document.querySelector(
+    "header .login-or-out a.egress"
+  ) as HTMLAnchorElement;
+  if (node != null) {
+    node.onclick = () => sessionStorage.clear();
+  }
+}
+
 async function legitUser() {
+  clickableLogoutLinkInHeader();
   const examId = sessionStorage.getItem("examId");
   if (typeof examId === "string" && examId.length > 0) {
     await showQText();
